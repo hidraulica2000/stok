1264d8b29c57423e4e83c4b6369ab19d293c2814
Edgar Ortega Ramirez <edgarortegaramirez@gmail.com>
2013-02-23 15:46:45 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hsc::Application.config.secret_token = 'cc9200c29d3374127081358d5041a3461caa7873afa0159fdb618cf607d05f83ca3efa3a0f7ead64d876ce00a7c6b3222f29bd0ebdafbfc8fe7d656e6a097674'
